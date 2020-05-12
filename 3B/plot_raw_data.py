5c6bd96d88d5db409780c1522e99fb9e368b2aa2
coreylammie <corey.lammie@jcu.edu.au>
2020-05-12 10:26:41 +1000

    Modified plot_raw_data.py

--- 3B/plot_raw_data.py
+++ 3B/plot_raw_data.py
@@ -3,19 +3,17 @@ import numpy as np
 import pandas as pd
 
 
-hrs = pd.read_csv('HRS.csv')
-lrs = pd.read_csv('LRS.csv')
+hrs = pd.read_csv('HRS_raw_data.csv')
+lrs = pd.read_csv('LRS_raw_data.csv')
 
 plt.figure(1)
 plt.xlim(1e0, 1e7)
 plt.ylim(1e4, 1e6)
 plt.grid()
-plt.plot(hrs.iloc[:, 0], hrs.iloc[:, 1], linestyle='', marker='.', color='b', markersize=10)
-plt.plot(lrs.iloc[:, 0], lrs.iloc[:, 1], linestyle='', marker='.', color='r', markersize=10)
+plt.plot(hrs.iloc[:, 0], hrs.iloc[:, 1], linestyle='', marker='.', color='b', markersize=15)
+plt.plot(lrs.iloc[:, 0], lrs.iloc[:, 1], linestyle='', marker='.', color='r', markersize=15)
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('Time (s)')
 plt.ylabel('R ($\Omega$)')
-
-
 plt.show()
