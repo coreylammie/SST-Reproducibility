454d7f9292b156d2dbf2519ecdd5844d8f37caea
coreylammie <corey.lammie@jcu.edu.au>
2020-05-12 12:06:59 +1000

    Updated plot_raw_data.py for Fig. 2A

--- 2A/plot_raw_data.py
+++ 2A/plot_raw_data.py
@@ -3,20 +3,22 @@ import numpy as np
 import pandas as pd
 
 
-# Currently hardcoded for 10
-lrs = pd.read_csv('LRS_10_raw_data.csv')
-hrs = pd.read_csv('HRS_10_raw_data.csv')
+lengths = [10, 20, 30]
+for index, length in enumerate(lengths):
+    lrs = pd.read_csv('LRS_%d_raw_data.csv' % length)
+    hrs = pd.read_csv('HRS_%d_raw_data.csv' % length)
+    plt.figure(index + 1)
+    plt.xlim(1e2, 1e9)
+    plt.ylim(1e3, 1e6)
+    plt.scatter(lrs.iloc[:, 0].values, lrs.iloc[:, 1].values, label='LRS', color='b')
+    plt.scatter(hrs.iloc[:, 0].values, hrs.iloc[:, 1].values, label='HRS', color='r')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('Device length = %dnm' % length)
+    plt.xlabel('Cycles')
+    plt.ylabel('Resistance ($\Omega$)')
+    plt.legend()
+    plt.grid(b=True, axis='both')
+    plt.minorticks_on()
 
-plt.figure(1)
-plt.xlim(1e2, 1e9)
-plt.ylim(1e3, 1e6)
-plt.scatter(lrs.iloc[:, 0].values, lrs.iloc[:, 1].values, label='LRS', color='b')
-plt.scatter(hrs.iloc[:, 0].values, hrs.iloc[:, 1].values, label='HRS', color='r')
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Cycles')
-plt.ylabel('Resistance ($\Omega$)')
-plt.legend()
-plt.grid(b=True, axis='both')
-plt.minorticks_on()
 plt.show()
