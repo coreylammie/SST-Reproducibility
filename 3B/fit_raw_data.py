48ec8f2de3c673df8e68628180a51cd30dde9d13
coreylammie <corey.lammie@jcu.edu.au>
2020-05-16 11:58:23 +1000

    Fit  Prelim Fig. 3B

--- 3B/fit_raw_data.py
+++ 3B/fit_raw_data.py
@@ -11,13 +11,15 @@ lrs = pd.read_csv('LRS_raw_data.csv')
 hrs = pd.read_csv('HRS_raw_data.csv')
 
 lrs_model = GeneralModel(operation_mode=OperationMode.gradual, tempurature_dependance=True, cell_size_dependance=False)
-# hrs_segment_model = GeneralModel(operation_mode=OperationMode.gradual, tempurature_dependance=True, cell_size_dependance=False)
+hrs_model = GeneralModel(operation_mode=OperationMode.gradual, tempurature_dependance=True, cell_size_dependance=False)
 lrs_model_parameters = lrs_model.fit(raw_data_x=lrs.iloc[:, 0].values,
                                      raw_data_y=lrs.iloc[:, 1].values,
                                      stable_resistance=9e4,
                                      threshold=1e3)
-
-print(lrs_model_parameters)
+hrs_model_parameters = hrs_model.fit(raw_data_x=hrs.iloc[:, 0].values,
+                                     raw_data_y=hrs.iloc[:, 1].values,
+                                     stable_resistance=330000,
+                                     threshold=1e3)
 
 plt.figure(1)
 plt.xlim(1e0, 1e7)
@@ -25,29 +27,12 @@ plt.ylim(1e4, 1e6)
 plt.scatter(lrs.iloc[:, 0], lrs.iloc[:, 1], label='LRS Experimental', color='b')
 plt.scatter(hrs.iloc[:, 0], hrs.iloc[:, 1], label='HRS Experimental', color='r')
 plt.scatter(lrs.iloc[:, 0].values, lrs_model.model(lrs.iloc[:, 0].values, **lrs_model_parameters), label='LRS Model', color='b', marker='s')
+plt.scatter(hrs.iloc[:, 0].values, hrs_model.model(hrs.iloc[:, 0].values, **hrs_model_parameters), label='HRS Model', color='r', marker='s')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('Time (s)')
 plt.ylabel('Resistance ($\Omega$)')
 plt.grid(b=True, axis='both')
 plt.minorticks_on()
+plt.legend()
 plt.show()
-
-
-
-
-
-
-#
-# plt.figure(1)
-# plt.xlim(1e0, 1e7)
-# plt.ylim(1e4, 1e6)
-# plt.scatter(lrs.iloc[:, 0], lrs.iloc[:, 1], label='LRS', color='b')
-# plt.scatter(hrs.iloc[:, 0], hrs.iloc[:, 1], label='HRS', color='r')
-# plt.xscale('log')
-# plt.yscale('log')
-# plt.xlabel('Time (s)')
-# plt.ylabel('Resistance ($\Omega$)')
-# plt.grid(b=True, axis='both')
-# plt.minorticks_on()
-# plt.show()
--- GeneralModel.py
+++ GeneralModel.py
@@ -53,10 +53,15 @@ class GeneralModel():
         concatenated_output = np.array([])
         concatenated_model_output = np.array([])
         number_of_sets = int(parameters['number_of_sets'])
-        for i in range(number_of_sets):
-            concatenated_output = np.append(concatenated_output, raw_data_y[i].flatten()).flatten()
-            model_output = self.model(raw_data_x[i], parameters['p_0'], parameters['p_1'], parameters['p_2_%d' % (i + 1)], parameters['p_3'], cell_size=parameters['cell_size_%d' % (i + 1)])
+        if number_of_sets == 1:
+            concatenated_output = np.append(concatenated_output, raw_data_y.flatten()).flatten()
+            model_output = self.model(raw_data_x, parameters['p_0'], parameters['p_1'], parameters['p_2_1'], parameters['p_3'], cell_size=parameters['cell_size_1'])
             concatenated_model_output = np.append(concatenated_model_output, model_output.flatten()).flatten()
+        else:
+            for i in range(number_of_sets):
+                concatenated_output = np.append(concatenated_output, raw_data_y[i].flatten()).flatten()
+                model_output = self.model(raw_data_x[i], parameters['p_0'], parameters['p_1'], parameters['p_2_%d' % (i + 1)], parameters['p_3'], cell_size=parameters['cell_size_%d' % (i + 1)])
+                concatenated_model_output = np.append(concatenated_model_output, model_output.flatten()).flatten()
 
         return np.abs(concatenated_model_output - concatenated_output)
 
@@ -125,8 +130,7 @@ class GeneralModel():
                 else:
                     parameters.add('p_2_%d' % (i + 1), value=0.5, expr='log(threshold_%d / p_1) / cell_size_%d and p_2_1' % (i + 1, i + 1))
 
-            print(parameters)
-            out = minimize(self.objective, parameters, args=(raw_data_x, raw_data_y)) #, method='dogbox')
+            out = minimize(self.objective, parameters, args=(raw_data_x, raw_data_y))
             return {'p_0': out.params['p_0'], 'p_1': out.params['p_1'], 'p_2': out.params['p_2_1'], 'p_3': out.params['p_3']}
         elif self.operation_mode == OperationMode.sudden:
             return {'p_0': np.log10(initial_resistance), 'p_1': np.log10(stable_resistance), 'p_2': threshold_slope, 'p_3': threshold_intercept}
