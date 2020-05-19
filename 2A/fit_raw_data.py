5a9f85e8e6f97e1eb71f49a0c245a498931cb543
Corey Lammie <corey.lammie@my.jcu.edu.au>
2020-05-19 12:13:51 +1000

    Stylized Figures and Removed Files to be Sorted  (#5)

--- 2A/fit_raw_data.py
+++ 2A/fit_raw_data.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,16 +10,21 @@ from GeneralModel import OperationMode
 
 
 lrs_10 = pd.read_csv('LRS_10_raw_data.csv')
+lrs_10 = lrs_10.sort_values(by=lrs_10.columns[0])
 lrs_20 = pd.read_csv('LRS_20_raw_data.csv')
+lrs_20 = lrs_20.sort_values(by=lrs_20.columns[0])
 lrs_30 = pd.read_csv('LRS_30_raw_data.csv')
+lrs_30 = lrs_30.sort_values(by=lrs_30.columns[0])
 hrs_10 = pd.read_csv('HRS_10_raw_data.csv')
+hrs_10 = hrs_10.sort_values(by=hrs_10.columns[0])
 hrs_20 = pd.read_csv('HRS_20_raw_data.csv')
+hrs_20 = hrs_20.sort_values(by=hrs_20.columns[0])
 hrs_30 = pd.read_csv('HRS_30_raw_data.csv')
+hrs_30 = hrs_30.sort_values(by=hrs_30.columns[0])
 lrs = [lrs_10, lrs_20, lrs_30]
 hrs = [hrs_10, hrs_20, hrs_30]
 cell_sizes = [10, 20, 30]
 
-# Cell size dependence
 lrs_model = GeneralModel(operation_mode=OperationMode.gradual, cell_size_dependance=True)
 hrs_model = GeneralModel(operation_mode=OperationMode.gradual, cell_size_dependance=True)
 lrs_model_parameters = lrs_model.fit(raw_data_x=[lrs_10.iloc[:, 0].values, lrs_20.iloc[:, 0].values],
@@ -32,48 +38,26 @@ hrs_model_parameters = hrs_model.fit(raw_data_x=[hrs_10.iloc[:, 0].values, hrs_2
                            threshold=[1e4, 1e7],
                            cell_size=[10, 20])
 
+matplotlib.rcParams['axes.linewidth'] = 2
+matplotlib.rcParams['font.family'] = 'sans-serif'
+label_size = 16
+tick_size = 12
 for i in range(len(cell_sizes)):
     plt.figure(i)
-    plt.grid()
+    plt.gca().set_axisbelow(True)
+    plt.minorticks_on()
+    plt.grid(b=True, which='both')
     plt.xlim(1e2, 1e9)
     plt.ylim(1e3, 1e6)
     plt.xscale('log')
     plt.yscale('log')
-    plt.title('Cell size = %dnm' % cell_sizes[i])
-    plt.scatter(lrs[i].iloc[:, 0].values, lrs[i].iloc[:, 1].values, label='LRS Experimental', color='b')
-    plt.scatter(hrs[i].iloc[:, 0].values, hrs[i].iloc[:, 1].values, label='HRS Experimental', color='r')
-    plt.scatter(lrs[i].iloc[:, 0].values, lrs_model.model(lrs[i].iloc[:, 0].values, **lrs_model_parameters, cell_size=cell_sizes[i]), label='LRS Model', color='b', marker='s')
-    plt.scatter(hrs[i].iloc[:, 0].values, hrs_model.model(hrs[i].iloc[:, 0].values, **hrs_model_parameters, cell_size=cell_sizes[i]), label='HRS Model', color='r', marker='s')
-    plt.legend()
-    plt.xlabel('Time (s)')
-    plt.ylabel('Resistance ($\Omega$)')
+    plt.title('(%dnm x %dnm)' % (cell_sizes[i], cell_sizes[i]), fontsize=label_size)
+    plt.plot(lrs[i].iloc[:, 0].values, lrs[i].iloc[:, 1].values, label='LRS Experimental', linestyle='-', color='b', marker='s', markersize=17.5, markerfacecolor='None', markeredgewidth=2.5)
+    plt.plot(hrs[i].iloc[:, 0].values, hrs[i].iloc[:, 1].values, label='HRS Experimental', linestyle='-', color='r', marker='s', markersize=17.5, markerfacecolor='None', markeredgewidth=2.5)
+    plt.plot(lrs[i].iloc[:, 0].values, lrs_model.model(lrs[i].iloc[:, 0].values, **lrs_model_parameters, cell_size=cell_sizes[i]), label='LRS Model', linestyle='--', color='b', marker='o', markersize=15, markerfacecolor='None', markeredgewidth=1)
+    plt.plot(hrs[i].iloc[:, 0].values, hrs_model.model(hrs[i].iloc[:, 0].values, **hrs_model_parameters, cell_size=cell_sizes[i]), label='HRS Model', linestyle='--', color='r', marker='o', markersize=15, markerfacecolor='None', markeredgewidth=1)
+    plt.xlabel('Cycle Number', fontsize=label_size)
+    plt.ylabel('Resistance ($\Omega$)', fontsize=label_size)
+    plt.gca().tick_params(axis='both', which='major', labelsize=tick_size)
 
-# Single cell size
-lrs_single_model = GeneralModel(operation_mode=OperationMode.gradual, cell_size_dependance=True)
-hrs_single_model = GeneralModel(operation_mode=OperationMode.gradual, cell_size_dependance=True)
-lrs_single_model_parameters = lrs_single_model.fit(raw_data_x=lrs_10.iloc[:, 0].values,
-                           raw_data_y=lrs_10.iloc[:, 1].values,
-                           stable_resistance=4400,
-                           threshold=1e4,
-                           cell_size=10)
-hrs_single_model_parameters = lrs_single_model.fit(raw_data_x=hrs_10.iloc[:, 0].values,
-                           raw_data_y=hrs_10.iloc[:, 1].values,
-                           stable_resistance=65000,
-                           threshold=1e4,
-                           cell_size=10)
-
-plt.figure(i + 1)
-plt.grid()
-plt.xlim(1e2, 1e9)
-plt.ylim(1e3, 1e6)
-plt.xscale('log')
-plt.yscale('log')
-plt.title('Cell size = %dnm' % cell_sizes[0])
-plt.scatter(lrs[0].iloc[:, 0].values, lrs[0].iloc[:, 1].values, label='LRS Experimental', color='b')
-plt.scatter(hrs[0].iloc[:, 0].values, hrs[0].iloc[:, 1].values, label='HRS Experimental', color='r')
-plt.scatter(lrs[0].iloc[:, 0].values, lrs_model.model(lrs[0].iloc[:, 0].values, **lrs_single_model_parameters, cell_size=cell_sizes[0]), label='LRS Model', color='b', marker='s')
-plt.scatter(hrs[0].iloc[:, 0].values, hrs_model.model(hrs[0].iloc[:, 0].values, **hrs_single_model_parameters, cell_size=cell_sizes[0]), label='HRS Model', color='r', marker='s')
-plt.legend()
-plt.xlabel('Time (s)')
-plt.ylabel('Resistance ($\Omega$)')
 plt.show()
--- 2B/fit_raw_data.py
+++ 2B/fit_raw_data.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,7 +9,9 @@ from GeneralModel import OperationMode
 
 
 lrs = pd.read_csv('LRS_raw_data.csv')
+lrs = lrs.sort_values(by=lrs.columns[0])
 hrs = pd.read_csv('HRS_raw_data.csv')
+hrs = hrs.sort_values(by=hrs.columns[0])
 
 lrs_model = GeneralModel(operation_mode=OperationMode.sudden, cell_size_dependance=False)
 hrs_model = GeneralModel(operation_mode=OperationMode.sudden, cell_size_dependance=False)
@@ -17,17 +20,23 @@ hrs_model_parameters = hrs_model.fit(initial_resistance=10.75e4, stable_resistan
 lrs_model_output = lrs_model.model(lrs.iloc[:, 0].values, **lrs_model_parameters)
 hrs_model_output = hrs_model.model(hrs.iloc[:, 0].values, **hrs_model_parameters)
 
+matplotlib.rcParams['axes.linewidth'] = 2
+matplotlib.rcParams['font.family'] = 'sans-serif'
+label_size = 16
+tick_size = 12
 plt.figure(1)
+plt.gca().set_axisbelow(True)
+plt.minorticks_on()
+plt.grid(b=True, which='both')
 plt.xlim(1e2, 1e8)
 plt.ylim(1e4, 12e4)
-plt.scatter(lrs.iloc[:, 0], lrs.iloc[:, 1], label='LRS Experimental', color='b')
-plt.scatter(hrs.iloc[:, 0], hrs.iloc[:, 1], label='HRS Experimental', color='r')
-plt.scatter(lrs.iloc[:, 0].values, lrs_model_output, label='LRS Model', color='b', marker='s')
-plt.scatter(hrs.iloc[:, 0].values, hrs_model_output, label='HRS Model', color='r', marker='s')
+plt.plot(lrs.iloc[:, 0], lrs.iloc[:, 1], label='LRS Experimental', linestyle='-', color='b', marker='s', markersize=17.5, markerfacecolor='None', markeredgewidth=2.5)
+plt.plot(hrs.iloc[:, 0], hrs.iloc[:, 1], label='HRS Experimental', linestyle='-', color='r', marker='s', markersize=17.5, markerfacecolor='None', markeredgewidth=2.5)
+plt.plot(lrs.iloc[:, 0].values, lrs_model_output, label='LRS Model', linestyle='--', color='b', marker='o', markersize=15, markerfacecolor='None', markeredgewidth=1)
+plt.plot(hrs.iloc[:, 0].values, hrs_model_output, label='HRS Model', linestyle='--', color='r', marker='o', markersize=15, markerfacecolor='None', markeredgewidth=1)
 plt.xscale('log')
-plt.xlabel('Time (s)')
-plt.ylabel('Resistance ($\Omega$)')
-plt.grid(b=True, axis='both')
-plt.minorticks_on()
-plt.legend()
+plt.xlabel('Time (s)', fontsize=label_size)
+plt.ylabel('Resistance ($\Omega$)', fontsize=label_size)
+plt.gca().tick_params(axis='both', which='major', labelsize=tick_size)
+
 plt.show()
