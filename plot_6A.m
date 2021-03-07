46ebb4ad4e2caffe9ef8daf9bf365490409ca43c
Corey Lammie <coreylammie@gmail.com>
2021-03-07 17:13:34 +1000

    Added plot_6A-D

---
+++ plot_6A.m
@@ -0,0 +1,22 @@
+figure(1);
+data = csvread('6A.csv', 1);
+x = log10(data(:, 1));
+y = data(:, 2);
+z = data(:, 3);
+dt = delaunayTriangulation(x,y) ;
+tri = dt.ConnectivityList ;
+xi = dt.Points(:,1) ; yi = dt.Points(:,2) ;
+F = scatteredInterpolant(x,y,z);
+zi = F(xi,yi);
+colormap(redblue);
+trisurf(tri,xi,yi,zi) 
+xlabel('Times Programmed');
+xtickformat('10^%d');
+xlim([1, 9]);
+xticks(1:9);
+ylim([1.3, 1.9]);
+ylabel('v_{stop}');
+zlabel('Test Set Accuracy (%)');
+zlim([40, 100]);
+title('TiN/Hf(Al)O/Hf/TiN'); 
+view(45, 45);
